ffd32e7a51460f0332798a9b39ef4c6a26b60a4d
Leonardo Cerqueira <leonevescer@gmail.com>
2025-09-28 11:05:55 -0300

    Learned Jinja syntax

--- market.py
+++ market.py
@@ -8,3 +8,13 @@ app = Flask(__name__)
 @app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/market')
+def market_page():
+    items=[
+        {'id':1, 'name':'RK Royal Kludge R75', 'barcode': '45378905678', 'price': 450},
+        {'id':2, 'name':'Masterkeys PRO M', 'barcode': '23568790653', 'price': 467},
+        {'id':3, 'name':'exbom BK152-C', 'barcode': '42696531729', 'price': 100},
+        {'id':4, 'name':'Mancer Shade MK2', 'barcode': '52123531729', 'price': 120}
+        ]
+    return render_template('market.html', items=items)
