b4365c6539a0fbe4e55bb706589ed12c02428d67
Leonardo Cerqueira <leonevescer@gmail.com>
2025-10-09 12:03:57 -0300

    Add forms and config secretkey

--- market/__init__.py
+++ market/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///market.db'
+app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
 db= SQLAlchemy(app)
 
 migrate=Migrate(app, db)
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
 from market import app  #<-- when we access the package it auto run __init__, tha's why we don't need to write market.__init__
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
 
 @app.route('/')
 @app.route('/home')
@@ -11,3 +12,15 @@ def home_page():
 def market_page():
     items=Item.query.all()
     return render_template('market.html', items=items)
+
+@app.route('/register')
+def register_page():
+    forms=RegisterForm()
+    return render_template('register.html', forms=forms)
+
+@app.route('/78805575')
+def img78805575_page():
+    return render_template('78805575.webp')
+
+                           
+
