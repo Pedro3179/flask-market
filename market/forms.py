83f389a98a85e7cab2068fa914d67cffd5efebe5
Leonardo Cerqueira <leonevescer@gmail.com>
2025-10-14 10:59:00 -0300

    Add form validations and error msgs

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
+
 
 class RegisterForm(FlaskForm):
-    username=StringField(label='User Name')
-    email_address= StringField(label='Email')
-    password1=PasswordField(label='Password')
-    password2=PasswordField(label='Confirm Password')
+    def validate_username(self, username_to_check):
+        user=User.query.filter_by(username=username_to_check.data).first()
+        if user is not None:
+            raise ValidationError('Username already exists. Please try a different username.')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address=User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address is not None:
+            raise ValidationError('Email already exists. Please try a different address.')
+        
+
+    username=StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
+    email_address= StringField(label='Email', validators=[Email(), DataRequired()])
+    password1=PasswordField(label='Password', validators=[Length(min=8), DataRequired()])
+    password2=PasswordField(label='Confirm Password', validators=[EqualTo('password1'), DataRequired()])
     submit=SubmitField(label='Create an Account')
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,6 @@
-from market import app  #<-- when we access the package it auto run __init__, tha's why we don't need to write market.__init__
-from flask import render_template
-from market.models import Item
+from market import app, db  #<-- when we access the package it auto run __init__, tha's why we don't need to write market.__init__
+from flask import render_template, url_for, redirect, flash
+from market.models import Item, User
 from market.forms import RegisterForm
 
 @app.route('/')
@@ -13,14 +13,19 @@ def market_page():
     items=Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register_page():
     forms=RegisterForm()
+    if forms.validate_on_submit():
+        user_to_create=User(username=forms.username.data,
+                            email_address=forms.email_address.data,
+                            password_hash=forms.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('market_page'))
+    if forms.errors != {}: # Print the error messages on the server side.
+        for error in forms.errors.values():
+            flash(f'Error creating user: {error}', category='danger')
     return render_template('register.html', forms=forms)
-
-@app.route('/78805575')
-def img78805575_page():
-    return render_template('78805575.webp')
-
                            
 
